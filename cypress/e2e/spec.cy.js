39609bb9e2d2a1410dd37c010714da0f3a10f81f
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-02-02 11:50:45 +0100

    wip(ci/cd): add script to cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://sveltejs.traefik.me');
+    cy.screenshot('first-page');
   })
 })
